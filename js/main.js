9f5882a26f5dbed8afed71bd81c3a169096e3a0d
Mevnt <tropik.555@mail.ru>
2024-05-26 11:21:29 +0300

    add: menu phone

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
-window.addEventListener('scroll', () => {
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
+window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
     logo.href.baseVal = "img/sprite.svg#logo";
@@ -8,4 +11,8 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove("navbar-light");
     logo.href.baseVal = "img/sprite.svg#logo-light";
   }
-}) 
+});
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.toggle("is-open");
+});
